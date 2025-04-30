d7e1f25d9b7db8d4316d4181dab4e3a7c0d45608
OmarMostafa888 <omar88mostafa8@gmail.com>
2025-04-30 14:42:36 +0300

    JS update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addBtn = document.querySelector(".add-btn");
 const taskList = document.querySelector(".task-list");
 
+let taskArr = [];
+
 function addTask() {
   if (document.getElementById("task-input").value.length >= 3) {
     const item = document.createElement("div");
@@ -23,22 +25,47 @@ function addTask() {
     label.appendChild(checkBoxText);
     checkBoxText.classList.add("checkbox-text");
     checkBoxText.innerText = document.getElementById("task-input").value;
+
+    const newTask = {
+      id: Date.now(),
+      title: document.getElementById("task-input").value,
+    };
+
+    taskArr.push(newTask);
+
+    console.log(newTask);
+    console.log(taskArr);
+    addToLocal();
     document.getElementById("task-input").value = "";
-    closeIcon.addEventListener("click", () => {
-      closeIcon.parentElement.remove();
-    });
+    ///////////////////////////////////////////////////////
   } else {
     alert("Please enter a task the contains at least 3 characters");
   }
 }
 
 let taskInput = document.getElementById("task-input");
+let closeIcons = document.querySelectorAll(".x-icon");
+taskList.addEventListener("click", (e) => {
+  if (e.target.innerText === "close") {
+    e.target.parentElement.remove();
+  }
+});
 
 function enterFunction(e) {
   if (e.code === "Enter") {
     addTask();
   }
 }
+
+function addToLocal() {
+  localStorage.setItem("tasks", JSON.stringify(taskArr));
+}
+
+function getFromLocal() {
+  JSON.parse(localStorage.getItem("tasks"));
+}
+
+getFromLocal();
 
 addBtn.addEventListener("click", addTask);
 taskInput.addEventListener("keydown", enterFunction);
